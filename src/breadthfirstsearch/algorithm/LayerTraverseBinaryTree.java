cc9644fd3173ad6d2711b3aa7daee536cbb92e3f
ShawnXxy <shawnxxy@hotmail.com>
2017-06-04 22:35:42 -0500

    BFS

---
+++ src/breadthfirstsearch/algorithm/LayerTraverseBinaryTree.java
@@ -0,0 +1,25 @@
+package breadthfirstsearch.algorithm;
+
+import java.util.List;
+
+public class LayerTraverseBinaryTree {
+	
+	class TreeNode {
+		public int key;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode (int key) {
+			this.key = key;
+		}
+	}
+
+	public List<Integer> getNodesLayerByLayer (TreeNode root) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tree/algorithm/CheckCompleted.java
@@ -0,0 +1,23 @@
+package tree.algorithm;
+
+public class CheckCompleted {
+	
+	class TreeNode {
+		public int key;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode (int key) {
+			this.key = key;
+		}
+	}
+	
+	public boolean isComplete (TreeNode root) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
