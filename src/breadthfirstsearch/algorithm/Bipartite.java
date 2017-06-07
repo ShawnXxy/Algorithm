28ca1576e49aa5848ed754808b402fc06db37ecc
ShawnXxy <shawnxxy@hotmail.com>
2017-06-07 17:20:59 -0500

    BFS

--- src/breadthfirstsearch/algorithm/Bipartite.java
+++ src/breadthfirstsearch/algorithm/Bipartite.java
@@ -1,6 +1,10 @@
 package breadthfirstsearch.algorithm;
 
 public class Bipartite {
+	
+	/*
+	 * Determine if an undirected graph is bipartite. A bipartite graph is one in which the nodes can be divided into two groups such that no nodes have direct edges to other nodes in the same group.
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/tree/algorithm/CheckCompleted.java
+++ src/tree/algorithm/CheckCompleted.java
@@ -1,5 +1,8 @@
 package tree.algorithm;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class CheckCompleted {
 	
 	class TreeNode {
@@ -12,7 +15,30 @@ public class CheckCompleted {
 	}
 	
 	public boolean isComplete (TreeNode root) {
-		
+		if (root ==  null) {
+			return true;
+		}
+		Deque<TreeNode> q = new LinkedList<>();
+		q.offer(root);
+		boolean flag = false;
+		while (!q.isEmpty()) {
+			TreeNode cur = q.poll();
+			if (cur.left == null) {			
+				flag = true;
+			} else if (flag) {
+				return false;
+			} else {
+				q.offer(cur.left);
+			}
+			if (cur.right == null) {
+				flag = true;
+			} else if (flag) {
+				return false;
+			} else {
+				q.offer(cur.right);
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
