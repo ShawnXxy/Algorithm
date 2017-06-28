862a6a83baef22dbf3e5f2fa4f8f4bb40cff5007
ShawnXxy <shawnxxy@hotmail.com>
2017-06-28 23:18:12 +0800

    probability

--- src/probabilityandsampling/Random7UsingRandom5.java
+++ src/probabilityandsampling/Random7UsingRandom5.java
@@ -1,5 +1,20 @@
 package probabilityandsampling;
 
 public class Random7UsingRandom5 {
+	
+	private int random5() {
+		return (int) (Math.random() * 5);
+	}
+	
+	public int random7() {
+		while (true) {
+			int n = 5 * random5() + random5();
+			if (n < 21) {
+				return n % 7;
+			}
+		}
+	}
+	
+		
 
 }
