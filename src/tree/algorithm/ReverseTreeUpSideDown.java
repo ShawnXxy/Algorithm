a6b18fcb1e73a47910e1ac44c403092c6030d0c5
ShawnXxy <shawnxxy@hotmail.com>
2017-08-05 10:16:25 +0800

    tree

---
+++ src/tree/algorithm/ReverseTreeUpSideDown.java
@@ -0,0 +1,31 @@
+package tree.algorithm;
+
+public class ReverseTreeUpSideDown {
+
+	class TreeNode {
+		public int key;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode (int key) {
+			this.key = key;
+		}
+	}
+	
+	public TreeNode reverse (TreeNode root) {
+		if (root == null || root.left == null) {
+			return root;
+		}
+		TreeNode newRoot = reverse(root.left);
+		root.left.left = root;
+		root.right.right = root.right;
+		root.left = null;
+		root.right = null;
+		return newRoot;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
